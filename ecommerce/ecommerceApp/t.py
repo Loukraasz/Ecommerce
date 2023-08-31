778be17c50059f9df0e38322db39e0d1c0a8b4eb
Loukraasz <peixotolukas06@gmail.com>
2023-08-31 19:03:57 -0300

    testes da conexao da api

---
+++ ecommerce/ecommerceApp/t.py
@@ -0,0 +1,6 @@
+class teste():
+    def __init__(self, name, stock, category, price):
+        self.name = name
+        self.stock = stock
+        self.category = category
+        self.price = price
--- ecommerce/ecommerceApp/views.py
+++ ecommerce/ecommerceApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "polls/index.html")
+def cad(request):
+    return render(request, "polls/cad.html")
+def login(request):
+    return render(request, "polls/login.html")
+

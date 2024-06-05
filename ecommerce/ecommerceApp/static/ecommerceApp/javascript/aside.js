131e8c7b9de43ddf21f6d922fca3056a263d8d57
Loukraasz <peixotolukas06@gmail.com>
2024-06-05 09:43:11 -0300

    ...

--- ecommerce/ecommerceApp/static/ecommerceApp/javascript/aside.js
+++ ecommerce/ecommerceApp/static/ecommerceApp/javascript/aside.js
@@ -1,19 +1,18 @@
-function setMargin(img1,img2,img3,img4,img5,img6){
+function setMargin(img1,img2,img3,img4){
     img1.style.display = "block";
     img2.style.display = "none";
     img3.style.display = "none";
     img4.style.display = "none";
-    img5.style.display = "none";
-    img6.style.display = "none";
+    
+
 
 }
-function removeMargin(img1,img2,img3,img4,img5,img6){
+function removeMargin(img1,img2,img3,img4){
     img1.style.display = "block";
     img2.style.display = "block";
     img3.style.display = "block";
     img4.style.display = "block";
-    img5.style.display = "block";
-    img6.style.display = "block";
+    
 }
 function setAside(aside){
     aside.style.display = "block";
@@ -22,15 +21,15 @@ function removeAside(aside){
     aside.style.display="none";
 
 }
-function set(img1,img2,img3,img4,img5,img6,info){
-        setMargin(img1,img2,img3,img4,img5,img6)
+function set(img1,img2,img3,img4,info){
+        setMargin(img1,img2,img3,img4)
         setAside(info)
     }
 function remove(info){
-    removeMargin(banner_court,banner_revolution,banner_max,banner_excee,banner_downshifter, banner_soon)
+    removeMargin(banner_court,banner_revolution,banner_excee,banner_downshifter)
     removeAside(info)
     }
-function onlyRight(img1,img2,img3,img4,img5,img6,info){
-    set(img1,img2,img3,img4,img5,img6,info)
+function onlyRight(img1,img2,img3,img4,info){
+    set(img1,img2,img3,img4,info)
 }
 
